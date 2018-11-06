b0566e5f1723507889a1d495ab8daea1fa527a12
gchicoye <gabriel.chicoye@gmail.com>
2018-11-06 22:43:48 +0100

    minor changes

--- .php_cs
+++ .php_cs
@@ -1,13 +1,10 @@
 <?php
-
 // for more info see:
 // https://github.com/FriendsOfPHP/PHP-CS-Fixer#usage
 // https://github.com/FriendsOfPHP/PHP-CS-Fixer/blob/master/UPGRADE.md
-
 $finder = PhpCsFixer\Finder::create()
 	->in(__DIR__)
 ;
-
 return PhpCsFixer\Config::create()
 	->setRules([
 		'@Symfony' => true,
--- script/hb/HeaderBiddingCreation.php
+++ script/hb/HeaderBiddingCreation.php
@@ -4,11 +4,21 @@ require __DIR__.'/../scriptLoader.php';
 
 use App\Scripts\HeaderBiddingScript;
 
+/*
+$buckets = 
+	["buckets" =>[
+		['precision' => 2, 'min' => 0, 'max' => 120, 'increment' => 0.40],
+		['precision' => 2, 'min' => 120, 'max' => 320, 'increment' => 2],
+		['precision' => 2, 'min' => 320, 'max' => 800, 'increment' => 20]
+	]
+]; 
+*/
+
 $entry = [
-	'ssp' => ['smartadserver'], // Needs to be bidder code defined in prebid documentation, ie appnexus, rubicon, improvedigital, smartadserver
+	'ssp' => ['ix'], // Needs to be bidder code defined in prebid documentation, ie appnexus, rubicon, improvedigital, smartadserver
 	'priceGranularity' => 'dense', // can be 'low', 'med', 'high', 'auto','dense', 'test'
 	'currency' => 'EUR',
-	'sizes' => [[728, 90], [1000, 200], [1000, 250], [1000, 300], [1000, 90], [120, 600], [160, 600], [300, 100], [300, 1000], [300, 250], [300, 50], [300, 600], [320, 100], [320, 50], [336, 280], [970, 150], [970, 250], [970, 90]],
+	'sizes' => [[970,250],[970,90],[728,250],[728,90],[320,50],[300,100],[250,250],[300,250],[300,600]]
 ];
 
 $script = new HeaderBiddingScript();
